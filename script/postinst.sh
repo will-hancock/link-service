2d4f98d9c6c1a8f8ed228bce7dcd2ed2ac9d005d
Linus Norton <linusnorton@gmail.com>
2017-03-16 16:41:12 +0000

    Added apache vhost

--- script/postinst.sh
+++ script/postinst.sh
@@ -7,8 +7,7 @@ then
 	echo "You must run this script from the script folder"
 	exit 1
 else
-    ENV=$(cat /etc/env)
-    sudo sed -e "s/\${env}/$ENV/" template.txt >> /etc/apache2/sites-available/link-service.conf
+    sudo ENV=$(cat /etc/env) sed -e "s/\${env}/$ENV/" ./script/apache2/link-service.conf >> /etc/apache2/sites-available/link-service.conf
     sudo a2ensite link-service
     sudo service apache2 reload
     sudo cp ./script/init.d/link-service.conf /etc/init/link-service.conf
