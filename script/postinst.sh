9af8eb87d0e0e1ecf3c0234523c2c1b25a9ac024
Linus Norton <linusnorton@gmail.com>
2017-03-16 16:47:35 +0000

    vhost

--- script/postinst.sh
+++ script/postinst.sh
@@ -8,7 +8,7 @@ then
 	exit 1
 else
     ENV=$(cat /etc/env)
-    sed -e "s/\${env}/$ENV/" ./script/apache2/link-service.conf >> /etc/apache2/sites-available/link-service.conf
+    sed -e "s/\${env}/$ENV/" ./script/apache2/link-service.conf > /etc/apache2/sites-available/link-service.conf
     a2ensite link-service
     service apache2 reload
     cp ./script/init.d/link-service.conf /etc/init/link-service.conf
