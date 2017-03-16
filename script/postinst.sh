73ca2114ae26648a8b6db2222ab4d88f27c2f50d
Linus Norton <linusnorton@gmail.com>
2017-03-16 09:57:10 +0000

    update postinstall

--- script/postinst.sh
+++ script/postinst.sh
@@ -1,54 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -e "/etc/env" ];
-then
-    env=`head -1 /etc/env`
-fi
-
-for arg in "$@"; do
-  shift
-  case "$arg" in
-    "--env") set -- "$@" "-e" ;;
-    "--name") set -- "$@" "-n" ;;
-    "--version") set -- "$@" "-v" ;;
-    "--task") set -- "$@" "-t" ;;
-    *)        set -- "$@" "$arg" ;;
-  esac
-done
-
-while getopts ":e:n:v:t:" opt; do
-  case $opt in
-    e)
-      env=$OPTARG;
-      ;;
-    n)
-      package_name=$OPTARG;
-      ;;
-    v)
-      version=$OPTARG;
-      ;;
-    t)
-      task=$OPTARG;
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
-  esac
-done
-
-echo "Package name: ${package_name} version: ${version} env: ${env} task: ${task}"
-
-if [ -z "$env" ]
-then
-	echo "--env is required parameter"
-	exit 1
-fi
-
 dir=`basename "$PWD"`;
 cd ../
 if [ "$dir" != "script" ]
