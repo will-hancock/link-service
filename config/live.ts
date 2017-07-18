0d8e9564ce42dc7624c4e921da951eda9928bd7f
Rafał Orłowski <orlowski@ifelse.pl>
2017-07-18 11:41:09 +0200

    CORE-484 live vhost and config

---
+++ config/live.ts
@@ -0,0 +1,48 @@
+import Config from './common';
+
+const DEFAULT_MAX_AGE = 10 * 60 * 60 * 24;
+
+export default class LiveConfig extends Config {
+
+    public baseUrl = 'http://$serviceName.live.aws.assertis';
+
+    public proxies = {
+        '^\/station\/.+': {
+            name: 'ride-service',
+            cacheConfig: {maxAge: DEFAULT_MAX_AGE, max: 4000},
+        },
+        '^\/ticket-type\/[a-zA-Z0-9]+': {
+            name: 'ride-service',
+            cacheConfig: {maxAge: DEFAULT_MAX_AGE, max: 500},
+        },
+        '^\/validity-type\/[a-zA-Z0-9]+': {
+            name: 'ride-service',
+            cacheConfig: {maxAge: DEFAULT_MAX_AGE, max: 100},
+        },
+        '^\/restriction\/[a-zA-Z0-9]+': {
+            name: 'ride-service',
+            cacheConfig: {maxAge: DEFAULT_MAX_AGE, max: 500},
+        },
+        '^\/route\/[0-9]+': {
+            name: 'ride-service',
+            cacheConfig: {maxAge: DEFAULT_MAX_AGE, max: 200},
+        },
+        '^\/railcard\/[a-zA-Z0-9]+': {
+            name: 'ride-service',
+            cacheConfig: {maxAge: DEFAULT_MAX_AGE, max: 20},
+        },
+        '^\/supplement-type\/[a-zA-Z0-9]+': {
+            name: 'ride-service',
+            cacheConfig: {maxAge: DEFAULT_MAX_AGE, max: 200},
+        },
+        '^\/delivery\/[a-zA-Z\-]+': {
+            name: 'delivery-service',
+            cacheConfig: {maxAge: DEFAULT_MAX_AGE, max: 200},
+        },
+        '^\/discount\/[0-9]+': {
+            name: 'discount-service',
+            cacheConfig: {maxAge: DEFAULT_MAX_AGE, max: 200},
+        },
+    };
+
+}
