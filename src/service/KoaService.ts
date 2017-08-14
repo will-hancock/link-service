9d5d89a5f525967477a42aeaceb72e43f3439161
Linus Norton <linusnorton@gmail.com>
2017-08-14 13:57:27 +0100

    remove lodash and fix the bug with the original impelementation

--- src/service/KoaService.ts
+++ src/service/KoaService.ts
@@ -5,7 +5,6 @@ import * as responseTime from 'koa-response-time';
 import {StatusCodeError} from 'request-promise/errors';
 import {CachedHttpClient} from '../http/CachedHttpClient';
 import {Logger} from '../logger/Logger';
-import {pick} from 'lodash';
 
 export default class KoaService {
 
@@ -65,4 +64,8 @@ export default class KoaService {
         }
     };
 
+}
+
+function pick(item, props) {
+    return Object.assign({}, ...props.map(prop => ({[prop]: item[prop]})));
 }
